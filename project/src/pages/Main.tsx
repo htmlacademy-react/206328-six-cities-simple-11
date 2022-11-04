eda514b9880b19ddc4c483817b80d6d929439b9f
Alexander <stdAlexTikhonov@hotmail.com>
2022-11-04 22:07:21 +0300

    fix(module2-task1): Main Updated

--- project/src/pages/Main.tsx
+++ project/src/pages/Main.tsx
@@ -2,11 +2,36 @@ import { Card } from '../components/card/Card';
 
 export const Main = () => (
   <>
+    <div style={{ display: 'none' }}>
+      <svg xmlns='http://www.w3.org/2000/svg'>
+        <symbol id='icon-arrow-select' viewBox='0 0 7 4'>
+          <path
+            fillRule='evenodd'
+            clipRule='evenodd'
+            d='M0 0l3.5 2.813L7 0v1.084L3.5 4 0 1.084V0z'
+          />
+        </symbol>
+        <symbol id='icon-bookmark' viewBox='0 0 17 18'>
+          <path d='M3.993 2.185l.017-.092V2c0-.554.449-1 .99-1h10c.522 0 .957.41.997.923l-2.736 14.59-4.814-2.407-.39-.195-.408.153L1.31 16.44 3.993 2.185z'></path>
+        </symbol>
+        <symbol id='icon-star' viewBox='0 0 13 12'>
+          <path
+            fillRule='evenodd'
+            clipRule='evenodd'
+            d='M6.5 9.644L10.517 12 9.451 7.56 13 4.573l-4.674-.386L6.5 0 4.673 4.187 0 4.573 3.549 7.56 2.483 12 6.5 9.644z'
+          />
+        </symbol>
+      </svg>
+    </div>
+
     <header className='header'>
       <div className='container'>
         <div className='header__wrapper'>
           <div className='header__left'>
-            <a className='header__logo-link header__logo-link--active'>
+            <a
+              className='header__logo-link header__logo-link--active'
+              href='https://github.com/'
+            >
               <img
                 className='header__logo'
                 src='img/logo.svg'
@@ -27,7 +52,7 @@ export const Main = () => (
                 </div>
               </li>
               <li className='header__nav-item'>
-                <a className='header__nav-link' href='#'>
+                <a className='header__nav-link' href='https://github.com/'>
                   <span className='header__signout'>Sign out</span>
                 </a>
               </li>
@@ -43,32 +68,50 @@ export const Main = () => (
         <section className='locations container'>
           <ul className='locations__list tabs__list'>
             <li className='locations__item'>
-              <a className='locations__item-link tabs__item' href='#'>
+              <a
+                className='locations__item-link tabs__item'
+                href='https://github.com/'
+              >
                 <span>Paris</span>
               </a>
             </li>
             <li className='locations__item'>
-              <a className='locations__item-link tabs__item' href='#'>
+              <a
+                className='locations__item-link tabs__item'
+                href='https://github.com/'
+              >
                 <span>Cologne</span>
               </a>
             </li>
             <li className='locations__item'>
-              <a className='locations__item-link tabs__item' href='#'>
+              <a
+                className='locations__item-link tabs__item'
+                href='https://github.com/'
+              >
                 <span>Brussels</span>
               </a>
             </li>
             <li className='locations__item'>
-              <a className='locations__item-link tabs__item tabs__item--active'>
+              <a
+                className='locations__item-link tabs__item tabs__item--active'
+                href='https://github.com/'
+              >
                 <span>Amsterdam</span>
               </a>
             </li>
             <li className='locations__item'>
-              <a className='locations__item-link tabs__item' href='#'>
+              <a
+                className='locations__item-link tabs__item'
+                href='https://github.com/'
+              >
                 <span>Hamburg</span>
               </a>
             </li>
             <li className='locations__item'>
-              <a className='locations__item-link tabs__item' href='#'>
+              <a
+                className='locations__item-link tabs__item'
+                href='https://github.com/'
+              >
                 <span>Dusseldorf</span>
               </a>
             </li>
@@ -84,6 +127,9 @@ export const Main = () => (
               <span className='places__sorting-caption'>Sort by</span>
               <span className='places__sorting-type' tabIndex={0}>
                 Popular
+                <svg className='places__sorting-arrow' width='7' height='4'>
+                  <use xlinkHref='#icon-arrow-select'></use>
+                </svg>
               </span>
               <ul className='places__options places__options--custom places__options--opened'>
                 <li
