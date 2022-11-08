7ddcf25fb17907aa69d8ac1cf9dabe0b9972bb5a
Alexander <stdAlexTikhonov@hotmail.com>
2022-11-08 23:03:17 +0300

    feat(module3-task1): Добавил роутинг

--- project/src/pages/not-found.tsx
+++ project/src/pages/not-found.tsx
@@ -1,1 +1,9 @@
-export const NotFound = () => <h1>Sorry page is not found 404 <a href="/">go back</a></h1>;
+import { Link } from 'react-router-dom';
+
+export const NotFound = () => (
+  <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', height: '100vh', justifyContent:'center'}}>
+    <h2>This is not the page you are looking for</h2>
+    <h1>404</h1>
+    <Link to="/" style={{ textDecoration: 'underline', color: 'blue'}}>go back</Link>
+  </div>
+);
