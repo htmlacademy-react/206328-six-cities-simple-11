40d8701617755161659172cabf2fceabecaac36f
Alexander <stdAlexTikhonov@hotmail.com>
2022-11-11 03:33:55 +0300

    feat(module4-task1): Добавил mock offers

--- project/src/index.tsx
+++ project/src/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './components/app/app';
+import { offers } from './mocks';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement,
@@ -11,6 +12,6 @@ const cards: Card[] = [1,2,3,4,5];
 
 root.render(
   <React.StrictMode>
-    <App cards={cards} />
+    <App cards={cards} offers={offers} />
   </React.StrictMode>,
 );
--- project/src/types/index.ts
+++ project/src/types/index.ts
@@ -1,3 +1,37 @@
 export type Card = number;
 export type MainProps = { cards: Card[] };
-export type AppProps = { cards: Card[] };
+
+export type Review = {
+  text: string;
+  name: string;
+  rating: number;
+  date: string;
+  avatar: string;
+};
+
+export type Host = {
+  name: string;
+  type: string;
+  avatar: string;
+  comments: string[];
+};
+
+export type Features = {
+  entire: string;
+  bedrooms: string;
+  adults: string;
+}
+
+export type Offer = {
+  name: string;
+  type: string;
+  features: Features;
+  rating: number;
+  price: number;
+  details: string[];
+  host: Host;
+  reviews: Review[];
+}
+
+
+export type AppProps = { cards: Card[]; offers: Offer[] };
