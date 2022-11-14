b14d8e0a741ade3a483f30328a13ad1d316400b9
Alexander <stdAlexTikhonov@hotmail.com>
2022-11-15 01:39:40 +0300

    feat(module5-task1): Добавил точки

--- project/src/mocks/offers.ts
+++ project/src/mocks/offers.ts
@@ -6,7 +6,7 @@ export const offers = [
     features: {
       entire: 'Apartment',
       bedrooms: '3 Bedrooms',
-      adults: 'Max 4 adults'
+      adults: 'Max 4 adults',
     },
     rating: 4.8,
     price: 120,
@@ -41,6 +41,10 @@ export const offers = [
         avatar: 'test.png',
       },
     ],
+    point: {
+      l: 52.3909553943508,
+      w: 4.85309666406198,
+    },
   },
   {
     id: 'e730b5b8-16fa-4cac-a04a-e09bbe280185',
@@ -49,7 +53,7 @@ export const offers = [
     features: {
       entire: 'Apartment',
       bedrooms: '3 Bedrooms',
-      adults: 'Max 4 adults'
+      adults: 'Max 4 adults',
     },
     rating: 4.8,
     price: 120,
@@ -84,6 +88,10 @@ export const offers = [
         avatar: 'test.png',
       },
     ],
+    point: {
+      l: 52.3609553943508,
+      w: 4.85309666406198,
+    },
   },
   {
     id: 'e730b5b8-16fa-4cac-a04a-e09bbe280185',
@@ -92,7 +100,7 @@ export const offers = [
     features: {
       entire: 'Apartment',
       bedrooms: '3 Bedrooms',
-      adults: 'Max 4 adults'
+      adults: 'Max 4 adults',
     },
     rating: 4.8,
     price: 120,
@@ -127,6 +135,10 @@ export const offers = [
         avatar: 'test.png',
       },
     ],
+    point: {
+      L: 52.3909553943508,
+      w: 4.929309666406198,
+    },
   },
   {
     id: 'e730b5b8-16fa-4cac-a04a-e09bbe280185',
@@ -135,7 +147,7 @@ export const offers = [
     features: {
       entire: 'Apartment',
       bedrooms: '3 Bedrooms',
-      adults: 'Max 4 adults'
+      adults: 'Max 4 adults',
     },
     rating: 4.8,
     price: 120,
@@ -170,91 +182,9 @@ export const offers = [
         avatar: 'test.png',
       },
     ],
-  },
-  {
-    id: 'e730b5b8-16fa-4cac-a04a-e09bbe280185',
-    name: 'Beautiful & luxurious studio at great location',
-    type: 'Premium',
-    features: {
-      entire: 'Apartment',
-      bedrooms: '3 Bedrooms',
-      adults: 'Max 4 adults'
+    point: {
+      l: 52.3809553943508,
+      w: 4.939309666406198,
     },
-    rating: 4.8,
-    price: 120,
-    details: [
-      'Wi-Fi',
-      'Washing machine',
-      'Towels',
-      'Heating',
-      'Coffee machine',
-      'Baby seat',
-      'Kitchen',
-      'Dishwasher',
-      'Cabel TV',
-      'Fridge',
-    ],
-    host: {
-      name: 'Angelina',
-      type: 'pro',
-      avatar: 'test.png',
-      comments: [
-        'A quiet cozy and picturesque that hides behind a a river by the unique lightness of Amsterdam. The building is green and from 18th century.',
-
-        'An independent House, strategically located between Rembrand Square and National Opera, but where the bustle of the city comes to rest in this alley flowery and colorful.',
-      ],
-    },
-    reviews: [
-      {
-        text: 'A quiet cozy and picturesque that hides behind a a river by the unique lightness of Amsterdam. The building is green and from 18th century.',
-        name: 'Max',
-        rating: 4,
-        date: '10.10.2022',
-        avatar: 'test.png',
-      },
-    ],
-  },
-  {
-    id: 'e730b5b8-16fa-4cac-a04a-e09bbe280185',
-    name: 'Beautiful & luxurious studio at great location',
-    type: 'Premium',
-    features: {
-      entire: 'Apartment',
-      bedrooms: '3 Bedrooms',
-      adults: 'Max 4 adults'
-    },
-    rating: 4.8,
-    price: 120,
-    details: [
-      'Wi-Fi',
-      'Washing machine',
-      'Towels',
-      'Heating',
-      'Coffee machine',
-      'Baby seat',
-      'Kitchen',
-      'Dishwasher',
-      'Cabel TV',
-      'Fridge',
-    ],
-    host: {
-      name: 'Angelina',
-      type: 'pro',
-      avatar: 'test.png',
-      comments: [
-        'A quiet cozy and picturesque that hides behind a a river by the unique lightness of Amsterdam. The building is green and from 18th century.',
-
-        'An independent House, strategically located between Rembrand Square and National Opera, but where the bustle of the city comes to rest in this alley flowery and colorful.',
-      ],
-    },
-    reviews: [
-      {
-        text: 'A quiet cozy and picturesque that hides behind a a river by the unique lightness of Amsterdam. The building is green and from 18th century.',
-        name: 'Max',
-        rating: 4,
-        date: '10.10.2022',
-        avatar: 'test.png',
-      },
-    ],
   },
 ];
