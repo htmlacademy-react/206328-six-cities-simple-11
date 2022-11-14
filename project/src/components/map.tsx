da16a2eea363734919a0b515ba1753e53a3fb01d
Alexander <stdAlexTikhonov@hotmail.com>
2022-11-15 02:25:45 +0300

    feat(module5-task1): Добавил маркеры

--- project/src/components/map.tsx
+++ project/src/components/map.tsx
@@ -2,12 +2,19 @@ import { useRef, useState, useEffect } from 'react';
 import leaflet from 'leaflet';
 import type { Map as MapType } from 'leaflet';
 import 'leaflet/dist/leaflet.css';
+import { URL_MARKER_DEFAULT } from './const';
+//URL_MARKER_CURRENT
 
 type City = {
   title: string;
   lat: number;
   lng: number;
   zoom: number;
+};
+
+type Point = {
+  lat: number;
+  lng: number;
 };
 
 const useMap = (
@@ -53,10 +60,58 @@ export const Map = () => {
     lat: 52.3609553943508,
     lng: 4.85309666406198,
     zoom: 10,
-
   };
 
-  useMap(mapRef, CITY);
+  const points: Point[] = [
+    {
+      lat: 52.3909553943508,
+      lng: 4.85309666406198,
+    },
+    {
+      lat: 52.3609553943508,
+      lng: 4.85309666406198,
+    },
+    {
+      lat: 52.3909553943508,
+      lng: 4.929309666406198,
+    },
+    {
+      lat: 52.3809553943508,
+      lng: 4.939309666406198,
+    },
+  ];
 
-  return <section className='cities__map map' ref={mapRef}></section>;
+  const defaultCustomIcon = leaflet.icon({
+    iconUrl: URL_MARKER_DEFAULT,
+    iconSize: [40, 40],
+    iconAnchor: [20, 40],
+  });
+
+  // const currentCustomIcon = leaflet.icon({
+  //   iconUrl: URL_MARKER_CURRENT,
+  //   iconSize: [40, 40],
+  //   iconAnchor: [20, 40],
+  // });
+
+  const map = useMap(mapRef, CITY);
+
+  useEffect(() => {
+    if (map) {
+      points.forEach((point) => {
+        leaflet
+          .marker(
+            {
+              lat: point.lat,
+              lng: point.lng,
+            },
+            {
+              icon: defaultCustomIcon,
+            }
+          )
+          .addTo(map);
+      });
+    }
+  }, [map, points]);
+
+  return <section className="cities__map map" ref={mapRef}></section>;
 };
