870213f5d8bd21f8acadb7e5d01cacade5ddd780
Alexander <stdAlexTikhonov@hotmail.com>
2022-11-18 00:33:07 +0300

    fix(module6-task1): Кладём данные в redux

--- project/src/components/app/app.tsx
+++ project/src/components/app/app.tsx
@@ -1,11 +1,21 @@
+import { useEffect } from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import { Login } from '../../pages/login';
 import { MainScreen } from '../../pages/main-screen';
 import { NotFound } from '../../pages/not-found';
 import { Room } from '../../pages/room';
 import type { AppProps } from '../../types';
+import { useAppDispatch } from '../../hooks';
+import { getCities, getOffers } from '../../store/action';
 
 function App({ offers }: AppProps): JSX.Element {
+  const dispatch = useAppDispatch();
+
+  useEffect(() => {
+    dispatch(getCities());
+    dispatch(getOffers());
+  });
+
   return (
     <BrowserRouter>
       <Routes>
