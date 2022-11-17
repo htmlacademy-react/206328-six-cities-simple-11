a998a963f5fae34527c2d7b6f4ed9149b8e70b5d
Alexander <stdAlexTikhonov@hotmail.com>
2022-11-17 17:55:39 +0300

    fix(module5-task2): Сделал общий компонент Map

--- project/src/components/map.tsx
+++ project/src/components/map.tsx
@@ -1,12 +1,57 @@
-import React from 'react';
-import { withMap } from '../hoc/with-map';
-import { MapComponentProps } from '../types';
+import { MapProps } from '../types';
+import { useRef, useEffect } from 'react';
+import leaflet, { Layer } from 'leaflet';
+import 'leaflet/dist/leaflet.css';
+import { useMap } from '../hooks/useMap';
 
-// eslint-disable-next-line react/display-name
-const MapComponent = React.forwardRef<HTMLElement, MapComponentProps>(
-  ({ className}, ref) => (
-    <section className={className} ref={ref}></section>
-  )
-);
+const URL_MARKER_DEFAULT = 'https://assets.htmlacademy.ru/content/intensive/javascript-1/demo/interactive-map/pin.svg';
+const URL_MARKER_CURRENT = 'https://assets.htmlacademy.ru/content/intensive/javascript-1/demo/interactive-map/main-pin.svg';
 
-export const Map = withMap(MapComponent);
+
+export const Map = ({ city, points, selectedPoint, className }: MapProps) => {
+  const mapRef = useRef(null);
+
+  const defaultCustomIcon = leaflet.icon({
+    iconUrl: URL_MARKER_DEFAULT,
+    iconSize: [40, 40],
+    iconAnchor: [20, 40],
+  });
+
+  const currentCustomIcon = leaflet.icon({
+    iconUrl: URL_MARKER_CURRENT,
+    iconSize: [40, 40],
+    iconAnchor: [20, 40],
+  });
+
+  const map = useMap(mapRef, city);
+
+  useEffect(() => {
+    let markers: Layer[];
+    if (map) {
+      markers = points.map((point) =>
+        leaflet
+          .marker(
+            {
+              lat: point.lat,
+              lng: point.lng,
+            },
+            {
+              icon:
+                point.lat === selectedPoint?.lat &&
+                point.lng === selectedPoint?.lng
+                  ? currentCustomIcon
+                  : defaultCustomIcon,
+            }
+          )
+          .addTo(map)
+      );
+    }
+    return () => {
+      markers?.forEach((marker: Layer) => {
+        map?.removeLayer(marker);
+      });
+    };
+  }, [map, points, selectedPoint, currentCustomIcon, defaultCustomIcon]);
+
+  return <section ref={mapRef} className={className} />;
+};
