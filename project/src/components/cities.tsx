43e8973ed47e98c844827ba77ed6e48b11ae0c5f
Alexander <stdAlexTikhonov@hotmail.com>
2022-11-19 13:37:16 +0300

    fix(module6-task1): Устанавливаем город

--- project/src/components/cities.tsx
+++ project/src/components/cities.tsx
@@ -1,18 +1,21 @@
-import { useAppSelector } from '../hooks';
+import { useAppSelector, useAppDispatch } from '../hooks';
+import { setCity } from '../store/action';
 
 export const Cities = () => {
   const cities = useAppSelector((state) => state.cities);
+  const selectedCty = useAppSelector((state) => state.selectedCity);
+  const dispatch = useAppDispatch();
 
   return (
     <ul className="locations__list tabs__list">
       {cities.map((item) => (
-        <li key={item.lat} className="locations__item">
-          <a
-            className="locations__item-link tabs__item"
-            href="https://localhost:3000"
+        <li key={item.title} className="locations__item">
+          <div
+            className={selectedCty?.title === item.title ? 'locations__item-link tabs__item tabs__item--active' : 'locations__item-link tabs__item'}
+            onClick={() => dispatch(setCity({ city: item}))}
           >
             <span>{item.title}</span>
-          </a>
+          </div>
         </li>
       ))}
     </ul>
