eff8c240971c0ec10dcec3cde2393122c3a927d7
Alexander <stdAlexTikhonov@hotmail.com>
2022-11-20 21:52:04 +0300

    fix(module6-task2): Изменяем sorting value

--- project/src/components/sorting-state.tsx
+++ project/src/components/sorting-state.tsx
@@ -2,32 +2,39 @@ import { useState } from 'react';
 
 export const SortingState = () => {
   const [open, setOpen] = useState(false);
-
+  const [sortingValue, setValue] = useState('Popular');
   const handleOpen = () => setOpen(true);
-  const handleChange = () => setOpen(false);
+  const handleChange = (value: string) => {
+    setOpen(false);
+    setValue(value);
+  };
+
+  const sortingTypes = [
+    'Popular',
+    'Price: low to high',
+    'Price: high to low',
+    'Top rated first',
+  ];
 
   return (
-    <form className="places__sorting" action="#" method="get">
-      <span className="places__sorting-caption">Sort by</span>
-      <span className="places__sorting-type" tabIndex={0} onClick={handleOpen}>
-        Popular
-        <svg className="places__sorting-arrow" width="7" height="4">
-          <use xlinkHref="#icon-arrow-select"></use>
+    <form className='places__sorting' action='#' method='get'>
+      <span className='places__sorting-caption'>Sort by</span>
+      <span className='places__sorting-type' tabIndex={0} onClick={handleOpen}>
+        {sortingValue}
+        <svg className='places__sorting-arrow' width='7' height='4'>
+          <use xlinkHref='#icon-arrow-select'></use>
         </svg>
       </span>
-      <ul className={`places__options places__options--custom${ open ? ' places__options--opened' : ''}`}>
-        <li className="places__option" tabIndex={0} onClick={handleChange}>
-          Popular
-        </li>
-        <li className="places__option" tabIndex={0} onClick={handleChange}>
-          Price: low to high
-        </li>
-        <li className="places__option" tabIndex={0} onClick={handleChange}>
-          Price: high to low
-        </li>
-        <li className="places__option" tabIndex={0} onClick={handleChange}>
-          Top rated first
-        </li>
+      <ul
+        className={`places__options places__options--custom${
+          open ? ' places__options--opened' : ''
+        }`}
+      >
+        {sortingTypes.map((item: string) => (
+          <li className='places__option' key={item} tabIndex={0} onClick={() => handleChange(item)}>
+            {item}
+          </li>
+        ))}
       </ul>
     </form>
   );
