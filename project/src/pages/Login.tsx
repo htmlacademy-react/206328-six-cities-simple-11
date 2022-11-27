f4e7e71b6b45205c9b752c4c762bb19a295e4868
Alexander <stdAlexTikhonov@hotmail.com>
2022-11-27 21:58:41 +0300

    fix(module7-task2): Обновил страницу Login

--- project/src/pages/Login.tsx
+++ project/src/pages/Login.tsx
@@ -1,64 +1,100 @@
-import { Link } from 'react-router-dom';
+import { useRef, FormEvent } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+import { useAppDispatch } from '../hooks';
+import { loginAction } from '../store/api-actions';
+import { AuthData } from '../types';
 
-export const Login = () => (
-  <div className='page page--gray page--login'>
+export const Login = () => {
+  const loginRef = useRef<HTMLInputElement | null>(null);
+  const passwordRef = useRef<HTMLInputElement | null>(null);
 
-    <header className='header'>
-      <div className='container'>
-        <div className='header__wrapper'>
-          <div className='header__left'>
-            <a className='header__logo-link' href='main.html'>
-              <img
-                className='header__logo'
-                src='img/logo.svg'
-                alt='6 cities logo'
-                width='81'
-                height='41'
-              />
-            </a>
+  const dispatch = useAppDispatch();
+  const navigate = useNavigate();
+
+  const onSubmit = (authData: AuthData) => {
+    dispatch(loginAction(authData));
+  };
+
+  const handleSubmit = (evt: FormEvent<HTMLFormElement>) => {
+    evt.preventDefault();
+
+    if (loginRef.current !== null && passwordRef.current !== null) {
+      onSubmit({
+        login: loginRef.current.value,
+        password: passwordRef.current.value,
+      });
+    }
+  };
+
+  return (
+    <div className='page page--gray page--login'>
+      <header className='header'>
+        <div className='container'>
+          <div className='header__wrapper'>
+            <div className='header__left'>
+              <a className='header__logo-link' href='main.html'>
+                <img
+                  className='header__logo'
+                  src='img/logo.svg'
+                  alt='6 cities logo'
+                  width='81'
+                  height='41'
+                />
+              </a>
+            </div>
           </div>
         </div>
-      </div>
-    </header>
+      </header>
 
-    <main className='page__main page__main--login'>
-      <div className='page__login-container container'>
-        <section className='login'>
-          <h1 className='login__title'>Sign in</h1>
-          <form className='login__form form' action='#' method='post'>
-            <div className='login__input-wrapper form__input-wrapper'>
-              <label className='visually-hidden'>E-mail</label>
-              <input
-                className='login__input form__input'
-                type='email'
-                name='email'
-                placeholder='Email'
-                required
-              />
+      <main className='page__main page__main--login'>
+        <div className='page__login-container container'>
+          <section className='login'>
+            <h1 className='login__title'>Sign in</h1>
+            <form
+              className='login__form form'
+              action='#'
+              onSubmit={handleSubmit}
+            >
+              <div className='login__input-wrapper form__input-wrapper'>
+                <label className='visually-hidden'>E-mail</label>
+                <input
+                  ref={loginRef}
+                  className='login__input form__input'
+                  type='email'
+                  name='email'
+                  placeholder='Email'
+                  required
+                />
+              </div>
+              <div className='login__input-wrapper form__input-wrapper'>
+                <label className='visually-hidden'>Password</label>
+                <input
+                  ref={passwordRef}
+                  className='login__input form__input'
+                  type='password'
+                  name='password'
+                  placeholder='Password'
+                  required
+                />
+              </div>
+              <button
+                onClick={() => navigate('/')}
+                className='login__submit form__submit button'
+                type='submit'
+              >
+                Sign in
+              </button>
+            </form>
+          </section>
+          <section className='locations locations--login locations--current'>
+            <div className='locations__item'>
+              <Link className='locations__item-link' to='/'>
+                <span>Paris</span>
+              </Link>
             </div>
-            <div className='login__input-wrapper form__input-wrapper'>
-              <label className='visually-hidden'>Password</label>
-              <input
-                className='login__input form__input'
-                type='password'
-                name='password'
-                placeholder='Password'
-                required
-              />
-            </div>
-            <button className='login__submit form__submit button' type='submit'>
-              Sign in
-            </button>
-          </form>
-        </section>
-        <section className='locations locations--login locations--current'>
-          <div className='locations__item'>
-            <Link className='locations__item-link' to="/">
-              <span>Paris</span>
-            </Link>
-          </div>
-        </section>
-      </div>
-    </main>
-  </div>
-);
+          </section>
+        </div>
+      </main>
+    </div>
+  );
+};
