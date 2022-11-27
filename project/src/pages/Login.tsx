85030455ac55c2be2d31a63a33801473539c055f
Alexander <stdAlexTikhonov@hotmail.com>
2022-11-27 21:46:35 +0300

    fix(module7-task2): Обновил страницу Login

--- project/src/pages/Login.tsx
+++ project/src/pages/Login.tsx
@@ -1,26 +1,7 @@
+import { Link } from 'react-router-dom';
+
 export const Login = () => (
   <div className='page page--gray page--login'>
-    <div style={{ display: 'none' }}>
-      <svg xmlns='http://www.w3.org/2000/svg'>
-        <symbol id='icon-arrow-select' viewBox='0 0 7 4'>
-          <path
-            fillRule='evenodd'
-            clipRule='evenodd'
-            d='M0 0l3.5 2.813L7 0v1.084L3.5 4 0 1.084V0z'
-          />
-        </symbol>
-        <symbol id='icon-bookmark' viewBox='0 0 17 18'>
-          <path d='M3.993 2.185l.017-.092V2c0-.554.449-1 .99-1h10c.522 0 .957.41.997.923l-2.736 14.59-4.814-2.407-.39-.195-.408.153L1.31 16.44 3.993 2.185z' />
-        </symbol>
-        <symbol id='icon-star' viewBox='0 0 13 12'>
-          <path
-            fillRule='evenodd'
-            clipRule='evenodd'
-            d='M6.5 9.644L10.517 12 9.451 7.56 13 4.573l-4.674-.386L6.5 0 4.673 4.187 0 4.573 3.549 7.56 2.483 12 6.5 9.644z'
-          />
-        </symbol>
-      </svg>
-    </div>
 
     <header className='header'>
       <div className='container'>
@@ -72,9 +53,9 @@ export const Login = () => (
         </section>
         <section className='locations locations--login locations--current'>
           <div className='locations__item'>
-            <a className='locations__item-link' href='http://localhost:3000'>
-              <span>Amsterdam</span>
-            </a>
+            <Link className='locations__item-link' to="/">
+              <span>Paris</span>
+            </Link>
           </div>
         </section>
       </div>
