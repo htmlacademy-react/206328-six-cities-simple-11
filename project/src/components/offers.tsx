86b2a50c14365f58caa7c84a0770da00ad6a8dc5
Alexander <stdAlexTikhonov@hotmail.com>
2022-12-11 00:40:32 +0300

    fix(module8-task2): Добавил компонент NoPlacesAvalible

--- project/src/components/offers.tsx
+++ project/src/components/offers.tsx
@@ -5,6 +5,7 @@ import { Map } from './map';
 import { useAppSelector } from '../hooks';
 import { SortingState } from './sorting-state';
 import { citySelector, offersSelector } from '../store/selectors';
+import { NoPlacesAvalible } from './no-places-avalible';
 
 export const Offers = (): JSX.Element => {
   const offers = useAppSelector(offersSelector);
@@ -21,36 +22,42 @@ export const Offers = (): JSX.Element => {
 
   return (
     <div className='cities'>
-      <div
-        className='cities__places-container container'
-      >
-        <section className='cities__places places' style={{ marginTop: 0, paddingBottom: 0 }}>
-          <h2 className='visually-hidden'>Places</h2>
-          <b className='places__found'>
-            {offers.length} places to stay in {city?.name}
-          </b>
-          <SortingState />
-          <div className='cities__places-list places__list tabs__content' style={{ height: 620, overflow: 'auto' }}>
-            {offers.map((offer: Offer) => (
-              <Card
-                key={offer.id}
-                offer={offer}
-                onListItemHover={onListItemHover}
+      {offers.length > 0 ? (
+        <div className='cities__places-container container'>
+          <section
+            className='cities__places places'
+            style={{ marginTop: 0, paddingBottom: 0 }}
+          >
+            <h2 className='visually-hidden'>Places</h2>
+            <b className='places__found'>
+              {offers.length} places to stay in {city?.name}
+            </b>
+            <SortingState />
+            <div
+              className='cities__places-list places__list tabs__content'
+              style={{ height: 620, overflow: 'auto' }}
+            >
+              {offers.map((offer: Offer) => (
+                <Card
+                  key={offer.id}
+                  offer={offer}
+                  onListItemHover={onListItemHover}
+                />
+              ))}
+            </div>
+          </section>
+          <div className='cities__right-section'>
+            {city && (
+              <Map
+                city={city}
+                points={points}
+                selectedPoint={selectedPoint}
+                className='cities__map map'
               />
-            ))}
+            )}
           </div>
-        </section>
-        <div className='cities__right-section'>
-          {city && (
-            <Map
-              city={city}
-              points={points}
-              selectedPoint={selectedPoint}
-              className='cities__map map'
-            />
-          )}
         </div>
-      </div>
+      ) : <NoPlacesAvalible />}
     </div>
   );
 };
