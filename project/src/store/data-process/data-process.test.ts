a7ad6a9baac4578cd6160cd357cd056590184bff
Alexander <stdAlexTikhonov@hotmail.com>
2022-12-17 23:18:09 +0300

    fix(module9-task1): Добавил тест загрузки предложений

--- project/src/store/data-process/data-process.test.ts
+++ project/src/store/data-process/data-process.test.ts
@@ -1,7 +1,18 @@
+import { loadOffers } from '../action';
+import { makeFakeOffers } from '../mocks';
 import { dataProcess, initialState } from './data-process';
+
+const offers = makeFakeOffers();
 
 describe('Reducer: DataProcess', () => {
   it('without additional parameters should return initial state', () => {
     expect(dataProcess.reducer(void 0, { type: 'UNKNOWN_ACTION '})).toEqual(initialState);
   });
+
+  it('should update questions by load hotels', () => {
+    const state = initialState;
+    expect(dataProcess.reducer(state, {type: loadOffers, payload: offers }))
+      .toEqual({...state, offers });
+  });
+
 });
