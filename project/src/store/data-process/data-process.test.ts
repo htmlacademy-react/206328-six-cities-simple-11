127c9cc2b956d1b19319db6b54732f455d607e3f
Alexander <stdAlexTikhonov@hotmail.com>
2022-12-17 23:36:55 +0300

    fix(module9-task1): Добавил тест для nearby

--- project/src/store/data-process/data-process.test.ts
+++ project/src/store/data-process/data-process.test.ts
@@ -1,24 +1,30 @@
-import { loadComments, loadOffers } from '../action';
-import { makeFakeOffers } from '../mocks';
+import { loadComments, loadNearby, loadOffers } from '../action';
+import { makeFakeOffers ,makeFakeComments } from '../mocks';
 import { dataProcess, initialState } from './data-process';
 
 const offers = makeFakeOffers();
+const comments = makeFakeComments();
 
 describe('Reducer: DataProcess', () => {
   it('without additional parameters should return initial state', () => {
     expect(dataProcess.reducer(void 0, { type: 'UNKNOWN_ACTION '})).toEqual(initialState);
   });
 
-  it('should update questions by load hotels', () => {
+  it('should update state by load hotels', () => {
     const state = initialState;
     expect(dataProcess.reducer(state, {type: loadOffers, payload: offers }))
       .toEqual({...state, offers });
   });
 
-  it('should update questions by load comments', () => {
+  it('should update state by load comments', () => {
     const state = initialState;
-    expect(dataProcess.reducer(state, {type: loadComments, payload: offers }))
-      .toEqual({...state, offers });
+    expect(dataProcess.reducer(state, {type: loadComments, payload: comments }))
+      .toEqual({...state, comments });
   });
 
+  it('should update state by load nearby', () => {
+    const state = initialState;
+    expect(dataProcess.reducer(state, {type: loadNearby, payload: offers }))
+      .toEqual({...state, nearby: offers });
+  });
 });
