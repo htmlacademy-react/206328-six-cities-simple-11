b75c04deb23f9cfc5b7bd5302f208996c4666b36
Alexander <stdAlexTikhonov@hotmail.com>
2022-12-17 03:49:00 +0300

    fix(module9-task1): Сделал тест для loginAction

--- project/src/store/api-actions.test.ts
+++ project/src/store/api-actions.test.ts
@@ -3,9 +3,11 @@ import thunk, {ThunkDispatch} from 'redux-thunk';
 import MockAdapter from 'axios-mock-adapter';
 import {configureMockStore} from '@jedmao/redux-mock-store';
 import {createAPI} from '../services/api';
-import {checkAuthAction} from './api-actions';
+import {checkAuthAction, loginAction} from './api-actions';
 import {APIRoute} from './const';
 import {State} from '../types/state';
+import { AuthData } from '../types';
+import { redirectToRoute } from './action';
 
 describe('Async actions', () => {
   const api = createAPI();
@@ -34,5 +36,31 @@ describe('Async actions', () => {
       checkAuthAction.pending.type,
       checkAuthAction.fulfilled.type
     ]);
+  });
+
+  it('should dispatch RequriedAuthorization and RedirectToRoute when POST /login', async () => {
+    const fakeUser: AuthData = {login: 'john@doe.ru', password: 'qwerty'};
+
+    mockAPI
+      .onPost(APIRoute.Login)
+      .reply(200, {token: 'xxx'});
+
+
+    const store = mockStore();
+    Storage.prototype.setItem = jest.fn();
+
+    await store.dispatch(loginAction(fakeUser));
+
+    const actions = store.getActions().map(({type}) => type);
+
+    expect(actions).toEqual([
+      loginAction.pending.type,
+      redirectToRoute.type,
+      loginAction.fulfilled.type
+    ]);
+
+    expect(Storage.prototype.setItem).toBeCalledTimes(2);
+    expect(Storage.prototype.setItem).toBeCalledWith('simle-city-token', 'xxx');
+    expect(Storage.prototype.setItem).toBeCalledWith('user-email', 'john@doe.ru');
   });
 });
